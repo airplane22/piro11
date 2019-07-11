d68fe2f3caa5cc4d35f9086cc93a07a23755adf1
airplane22 <52434831+airplane22@users.noreply.github.com>
2019-07-11 10:16:03 +0900

    2주차 수요일 과제 문제01

--- 2주차 수요일 과제/문제01.py
+++ 2주차 수요일 과제/문제01.py
@@ -1,20 +1,37 @@
-words = ['dog','dogs','doge']
+words = ['hawaii','happy']
 
 words.sort(key=len)
 
-for i in range(0, len(words[0])+1):
-        for j in range(1, len(words)):
-                if words[j][0:i] == words[0][0:i] :
-                        x = words[0][0:i]
-                else :
-                        break
+x=''
 
-        
-print(x)
+breaking = 1
                         
+for i in range(0, len(words[0])):
+        for j in range(1, len(words)):
 
+
+                if words[j][0:i+1] == words[0][0:i+1] :
+                        x = words[0][0:i+1]
+                        
+                elif words[j][0:i+1] != words[0][0:i+1]:
+                        if i>= 0:
+                                x = words[0][0:i]
+
+                                breaking = 0
+                                break
+                                
+                        else :
+                                x = 100
+                                breaking = 0
+                                break
+        if breaking == 0:
+                break
+                        
+                        
+        
+        
                
-               
+print(x)               
 
 
 
