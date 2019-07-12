1b0827a65281e83997d7e45c10cc8c54cc06cc89
airplane22 <52434831+airplane22@users.noreply.github.com>
2019-07-12 16:05:56 +0900

    2주차 수요일과제 문제 02

--- 2주차 수요일 과제/문제02.py
+++ 2주차 수요일 과제/문제02.py
@@ -1,20 +1,25 @@
-x = input('캐릭터의 이름을 입력하세요: ')
+class character:
+        def __init__(self,name):
+                self.name = name
+                self.힘 = '힘'
+                self.지력 = '지력'
+                self.직업 = '직업'
 
+        def info(self):
+                l = [6, 7, 8]
+                import random
+                self.힘= random.choice(l)
+                self.지력= random.choice(l)
+                if self.힘>self.지력 :
+                        self.직업 = '전사'
+                elif self.힘==self.지력 :
+                        self.직업 = '궁수'
+                else :
+                        self.직업 = '법사'
+                        print('캐릭터 이름: ', self.name)
+                        print('캐릭터 정보: 힘(',self.힘,'), 지력(',self.지력,')')
+                        print('캐릭터 직업: ',self.직업)
 
-l = [6, 7, 8]
-import random
-힘= random.choice(l)
-지력= random.choice(l)
-
-if 힘>지력 :
-        직업 = '전사'
-elif 힘==지력 :
-        직업 = '궁수'
-else :
-        직업 = '법사'
-
-print('캐릭터 이름: ', x)
-print('캐릭터 정보: 힘(',힘,'), 지력(',지력,')')
-print('캐릭터 직업: ',직업)
-
+player = character(input('캐릭터의 이름을 입력하세요: '))
+player.info()
         
